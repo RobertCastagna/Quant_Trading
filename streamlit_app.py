b62ae987ef843c787f675fe1f3b0c56c67372fd2
Robert Castagna <49927397+RobertCastagna@users.noreply.github.com>
2023-09-15 12:50:10 -0400

    p1

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,9 +21,43 @@ ticker = st.selectbox(
     'Which Security are we lookin at losing money on today?',
     (options))
 
+
+
+
+# get only todays data and post dataframe of live open, close, etc..
+
+tickerData = yf.Ticker(ticker)
+Data = tickerData.history(period='2d',interval='5m')
+
+todayData = Data.reset_index()
+
+todayData['Datetime'] = todayData['Datetime'].dt.tz_localize(None) 
+todayData['TimeOfDay'] = todayData['Datetime'].apply(lambda x: pd.Timestamp(x))
+
+today = dt.datetime.today()
+filtered_df = todayData[todayData['Datetime'] > today - dt.timedelta(1)]
+filtered_df['TimeOfDay'] = filtered_df['TimeOfDay'].dt.time
+filtered_df['TimeOfDay'] = filtered_df['TimeOfDay'].apply(lambda x: str(x))
+filtered_df = filtered_df.set_index('TimeOfDay').drop('Datetime', axis = 1)
+
+daily_output_data = filtered_df.iloc[0].T
+st.dataframe(daily_output_data)
+
+fig, ax1= plt.subplots()
+plt.title(f"Today's {ticker} Data")
+plt.style.use('fivethirtyeight')
+plt.xticks(rotation=90)
+ax1.set_xticklabels(labels=filtered_df.index, fontdict={"fontsize": 5})
+ax1.plot(filtered_df['Close'])
+
+st.pyplot(fig)
+
+
+
+
 lookback_duration = st.number_input("How many calendar days of history do you want to chart and test?", min_value=90, max_value=365, step=7)
 
-# get data for that security and timeframe
+# get data for input security and timeframe
 
 today = date.today() - dt.timedelta(lookback_duration)
 one_month_lag_date = today.strftime('%Y-%m-%d')
@@ -50,38 +84,13 @@ indicator_data['Date'] = indicator_data['Date'].dt.tz_localize(None)
 indicator_data['Date'] = indicator_data['Date'].apply(lambda x: pd.Timestamp(x))
 indicator_data['Date'] = indicator_data['Date'].dt.date
 stock = indicator_data.set_index('Date')
-#st.dataframe(stock['RSI','MACD'])
+st.dataframe(stock['RSI','MACD'])
 
 
 
 
 
-# get only todays data and post dataframe of live open, close, etc..
 
-tickerData = yf.Ticker(ticker)
-Data = tickerData.history(period='2d',interval='5m')
-
-todayData = Data.reset_index()
-
-todayData['Datetime'] = todayData['Datetime'].dt.tz_localize(None) 
-todayData['TimeOfDay'] = todayData['Datetime'].apply(lambda x: pd.Timestamp(x))
-
-today = dt.datetime.today()
-filtered_df = todayData[todayData['Datetime'] > today - dt.timedelta(1)]
-filtered_df['TimeOfDay'] = filtered_df['TimeOfDay'].dt.time
-filtered_df['TimeOfDay'] = filtered_df['TimeOfDay'].apply(lambda x: str(x))
-filtered_df = filtered_df.set_index('TimeOfDay').drop('Datetime', axis = 1)
-
-st.dataframe(filtered_df.iloc[0].T)
-
-fig, ax1= plt.subplots()
-plt.title(f"Today's {ticker} Data")
-plt.style.use('fivethirtyeight')
-plt.xticks(rotation=90)
-ax1.set_xticklabels(labels=filtered_df.index, fontdict={"fontsize": 5})
-ax1.plot(filtered_df['Close'])
-
-st.pyplot(fig)
 
 
 
