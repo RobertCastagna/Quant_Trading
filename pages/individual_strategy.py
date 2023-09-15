18f6ce5862ef04900929d8a8e5dd7c040abd73b8
Robert Castagna <49927397+RobertCastagna@users.noreply.github.com>
2023-09-15 12:29:03 -0400

    app test

--- pages/individual_strategy.py
+++ pages/individual_strategy.py
@@ -11,8 +11,7 @@ from backtesting.test import SMA
 from datetime import date
 from backtest import MACD, MeanReversion, SwingTrading, RsiOscillator
 
-# print daily chart
-
+# pick ticker and time interval
 ticker_options = pd.read_excel('indicators.xlsx')
 
 options = ', '.join(ticker_options['tickers'])
@@ -21,35 +20,12 @@ ticker = st.selectbox(
     'Which Security are we lookin at losing money on today?',
     (options))
 
-tickerData = yf.Ticker(ticker)
-Data = tickerData.history(period='30d',interval='5m')
-
-todayData = Data.reset_index()
-todayData = todayData[['Datetime','Close']]
-
-todayData['Datetime'] = todayData['Datetime'].dt.tz_localize(None) 
-todayData['TimeOfDay'] = todayData['Datetime'].apply(lambda x: pd.Timestamp(x))
-
-today = dt.datetime.today()
-filtered_df = todayData[todayData['Datetime'] > today - dt.timedelta(1)]
-filtered_df['TimeOfDay'] = filtered_df['TimeOfDay'].dt.time
-filtered_df['TimeOfDay'] = filtered_df['TimeOfDay'].apply(lambda x: str(x))
-filtered_df = filtered_df.set_index('TimeOfDay').drop('Datetime', axis = 1)
-
-fig, ax1= plt.subplots()
-plt.title(f"Today's {ticker} Data")
-plt.style.use('fivethirtyeight')
-plt.xticks(rotation=90)
-ax1.set_xticklabels(labels=filtered_df.index, fontdict={"fontsize": 5})
-ax1.plot(filtered_df['Close'])
-
-st.pyplot(fig)
-
-
-#   this will be the run through each of our strategies and a coloured button will be green or red if it has shown a buy signal on the day
 
 lookback_duration = st.number_input("How many calendar days of history do you want to chart and test?", min_value=90, max_value=365, step=7)
 
+
+
+# get data based on time interval until today
 today = date.today() - dt.timedelta(lookback_duration)
 one_month_lag_date = today.strftime('%Y-%m-%d')
 
@@ -57,8 +33,10 @@ stock = yf.download(ticker, start=one_month_lag_date)[
     ["Open", "High", "Low", "Close", "Volume"]
 ].reset_index()
 
+# pick the strategy and only run code for a given strategy
+strat = st.selectbox("pick a strategy:", ('','SwingTrading', 'MACD', 'MeanReversion', 'RsiOscillator'))
 
-# Calculate the indicators using TA-Lib
+# Calculate the RSI indicator 
 macd, signal, hist = ta.MACD(stock['Close'], fastperiod=12, slowperiod=26, signalperiod=9)
 daily_rsi = ta.RSI(stock['Close'], 14)
 upper_bound = RsiOscillator.upper_bound
@@ -75,5 +53,28 @@ stock['Date'] = stock['Date'].apply(lambda x: pd.Timestamp(x))
 stock['Date'] = stock['Date'].dt.date
 #stock = stock[stock['MACD'].notna()]
 stock = stock.set_index('Date')
+
+
+# Plot RSI
+fig_rsi, ax_rsi= plt.subplots()
+plt.xticks(rotation=90)
+ax_rsi.plot(stock['RSI'], label='RSI')
+ax_rsi.set_xticklabels(labels=stock.index, fontdict={"fontsize": 8})
+plt.legend()
+ax_rsi.axhline(y=lower_bound, color='red', linestyle='--')
+ax_rsi.axhline(y=upper_bound, color='green', linestyle='--')
+st.pyplot(fig_rsi)
+
+# Plot MACD
+fig_macd, ax_macd= plt.subplots()
+plt.xticks(rotation=90)
+ax_macd.plot(stock['MACD'], label='MACD')
+ax_macd.plot(stock['signal'], label='Signal')
+ax_macd.bar(stock.index, stock['hist'], color = 'green', tick_label = stock.index)
+ax_macd.set_xticklabels(labels=stock.index, fontdict={"fontsize": 8})
+plt.legend()
+ax_macd.axhline(y=0, color='black', linestyle='--')
+st.pyplot(fig_macd)
+
 
 
